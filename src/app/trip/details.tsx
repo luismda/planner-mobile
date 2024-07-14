0441b43c5799792fa3206421cfe1f80b3f4baf03
Luís Miguel <luismigueldutraalves@gmail.com>
2024-07-14 16:38:00 -0300

    fix: adjust top spacing to trip details screen

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -1,6 +1,6 @@
 import { Link2, Plus, Tag, UserCog } from 'lucide-react-native'
 import { useState } from 'react'
-import { Alert, Text, View } from 'react-native'
+import { Alert, ScrollView, Text, View } from 'react-native'
 import Animated, { SlideInRight, SlideOutRight } from 'react-native-reanimated'
 
 import { Button } from '@/components/button'
@@ -70,86 +70,90 @@ export function Details({ tripId }: DetailsProps) {
   const participants = tripParticipants?.participants ?? []
 
   return (
-    <Animated.ScrollView
-      showsVerticalScrollIndicator={false}
+    <Animated.View
+      className="flex-1 pt-5"
       entering={SlideInRight.duration(400)}
       exiting={SlideOutRight.duration(400)}
-      contentContainerClassName="flex-1 gap-5 pt-5 pb-48"
     >
-      <View className="gap-6">
-        <Text className="font-semibold text-xl text-zinc-50">
-          Links importantes
-        </Text>
-
-        <View className="gap-5">
-          {links.length > 0 ? (
-            links.map((link) => <TripLink key={link.id} data={link} />)
-          ) : (
-            <Text className="font-regular text-sm text-zinc-500">
-              Nenhum link adicionado.
-            </Text>
-          )}
-        </View>
-
-        <Button
-          variant="secondary"
-          onPress={() => setVisibleModal(VisibleModalEnum.NEW_LINK)}
-        >
-          <Plus color={colors.zinc[200]} size={20} />
-          <Button.Title>Cadastrar novo link</Button.Title>
-        </Button>
-      </View>
-
-      <View className="h-px bg-zinc-800" />
-
-      <View className="gap-6">
-        <Text className="font-semibold text-xl text-zinc-50">Convidados</Text>
-
-        <View className="gap-5">
-          {participants.map((participant) => (
-            <Participant key={participant.id} data={participant} />
-          ))}
-        </View>
-
-        <Button variant="secondary">
-          <UserCog color={colors.zinc[200]} size={20} />
-          <Button.Title>Gerenciar convidados</Button.Title>
-        </Button>
-      </View>
-
-      <Modal
-        title="Cadastrar link"
-        subtitle="Todos os convidados podem visualizar os links importantes."
-        visible={visibleModal === VisibleModalEnum.NEW_LINK}
-        onClose={() => setVisibleModal(VisibleModalEnum.NONE)}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="flex-grow-1 gap-5 pb-48"
       >
-        <View className="mb-3 mt-5 gap-2">
-          <Input variant="secondary">
-            <Tag color={colors.zinc[400]} size={20} />
+        <View className="gap-6">
+          <Text className="font-semibold text-xl text-zinc-50">
+            Links importantes
+          </Text>
 
-            <Input.Field
-              value={linkTitle}
-              placeholder="Título do link"
-              onChangeText={setLinkTitle}
-            />
-          </Input>
+          <View className="gap-5">
+            {links.length > 0 ? (
+              links.map((link) => <TripLink key={link.id} data={link} />)
+            ) : (
+              <Text className="font-regular text-sm text-zinc-500">
+                Nenhum link adicionado.
+              </Text>
+            )}
+          </View>
 
-          <Input variant="secondary">
-            <Link2 color={colors.zinc[400]} size={20} />
-
-            <Input.Field
-              value={linkUrl}
-              placeholder="URL"
-              keyboardType="url"
-              onChangeText={setLinkUrl}
-            />
-          </Input>
+          <Button
+            variant="secondary"
+            onPress={() => setVisibleModal(VisibleModalEnum.NEW_LINK)}
+          >
+            <Plus color={colors.zinc[200]} size={20} />
+            <Button.Title>Cadastrar novo link</Button.Title>
+          </Button>
         </View>
 
-        <Button isLoading={isCreatingTripLink} onPress={handleCreateTripLink}>
-          <Button.Title>Salvar link</Button.Title>
-        </Button>
-      </Modal>
-    </Animated.ScrollView>
+        <View className="h-px bg-zinc-800" />
+
+        <View className="gap-6">
+          <Text className="font-semibold text-xl text-zinc-50">Convidados</Text>
+
+          <View className="gap-5">
+            {participants.map((participant) => (
+              <Participant key={participant.id} data={participant} />
+            ))}
+          </View>
+
+          <Button variant="secondary">
+            <UserCog color={colors.zinc[200]} size={20} />
+            <Button.Title>Gerenciar convidados</Button.Title>
+          </Button>
+        </View>
+
+        <Modal
+          title="Cadastrar link"
+          subtitle="Todos os convidados podem visualizar os links importantes."
+          visible={visibleModal === VisibleModalEnum.NEW_LINK}
+          onClose={() => setVisibleModal(VisibleModalEnum.NONE)}
+        >
+          <View className="mb-3 mt-5 gap-2">
+            <Input variant="secondary">
+              <Tag color={colors.zinc[400]} size={20} />
+
+              <Input.Field
+                value={linkTitle}
+                placeholder="Título do link"
+                onChangeText={setLinkTitle}
+              />
+            </Input>
+
+            <Input variant="secondary">
+              <Link2 color={colors.zinc[400]} size={20} />
+
+              <Input.Field
+                value={linkUrl}
+                placeholder="URL"
+                keyboardType="url"
+                onChangeText={setLinkUrl}
+              />
+            </Input>
+          </View>
+
+          <Button isLoading={isCreatingTripLink} onPress={handleCreateTripLink}>
+            <Button.Title>Salvar link</Button.Title>
+          </Button>
+        </Modal>
+      </ScrollView>
+    </Animated.View>
   )
 }
